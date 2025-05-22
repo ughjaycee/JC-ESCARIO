500ad448a2c2467c391ea7e811fa5143937a19cf
JC Escario <johncarlescario196@gmail.com>
2025-05-22 14:36:15 +0800

    Updated List and Updated Sum

--- sum_even_numbers.py
+++ sum_even_numbers.py
@@ -8,5 +8,6 @@ def sum_even(numbers):
     return total
 
 # Example usage
-sample_list = [1, 2, 3, 4, 5, 6]
+sample_list = [1, 2, 3, 4, 5, 6, 8, 10, 12, 14, 16]
+print("List", sample_list)
 print("Sum of even numbers:", sum_even(sample_list))
