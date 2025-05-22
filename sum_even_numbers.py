f162d9be16e5660d3faee3876454b24381acb4ae
JC Escario <johncarlescario196@gmail.com>
2025-05-22 14:04:24 +0800

    Updated Image operation day5

--- sum_even_numbers.py
+++ sum_even_numbers.py
@@ -1,12 +1,13 @@
-# sum_even_numbers.py
-
-def sum_even(numbers):
+def sum_even_numbers(numbers):
+    """Returns the sum of all even numbers in the list."""
     total = 0
     for num in numbers:
         if num % 2 == 0:
             total += num
     return total
 
-# Example usage
-sample_list = [1, 2, 3, 4, 5, 6]
-print("Sum of even numbers:", sum_even(sample_list))
+if __name__ == "main":
+
+    sample_list = [1, 2, 3, 4, 5, 6]
+    print("List:", sample_list)
+    print("Sum of even numbers:", sum_even_numbers(sample_list))
